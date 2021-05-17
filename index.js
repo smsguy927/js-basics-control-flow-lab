7b5b0359aad14fa2eeea1e48fefb7677e92bc000
smsguy927 <smsguy927@gmail.com>
2021-05-17 13:57:40 -0500

    Done

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  const FREE_MAX = 400;
+  const NO_SURCHARGE_MAX = 2000;
+  const DISTANCE_MAX = 2500;
+  if (feet <= FREE_MAX) {
+    return "This one is on me!";
+  } else if (feet > NO_SURCHARGE_MAX && feet <= DISTANCE_MAX) {
+    return "I will gladly take your thirty bucks.";
+  } else if (feet > DISTANCE_MAX) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let resultMessage;
+  switch (tip) {
+    case "generous":
+      resultMessage = "Thank you so much.";
+      break;
+
+    case "not as generous":
+      resultMessage = "Thank you.";
+      break;
+
+    default:
+      resultMessage = "Bye.";
+      break;
+  }
+  return resultMessage;
 }
